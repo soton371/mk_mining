1066deea6501c056b656fb0d03a36749e7249108
Soton <soton371@gmail.com>
2023-10-20 11:21:54 +0600

    reset password

---
+++ lib/views/auth/new_password_scr.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mk_mining/configs/colors.dart';
+import 'package:mk_mining/configs/sizes.dart';
+import 'package:mk_mining/views/auth/sign_in_scr.dart';
+
+class NewPasswordScreen extends StatelessWidget {
+  const NewPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover)),
+        ),
+
+        ListView(
+          padding: const EdgeInsets.symmetric(horizontal: AppSizes.bodyPadding),
+          children: [
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            Image.asset(
+              "assets/images/reset-password.png",
+              height: AppSizes.width(context) * 0.25,
+            ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            Column(
+              children: [
+                Text(
+                  "Reset Password",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: AppSizes.width(context) * 0.07),
+                ),
+                const Text(
+                  "Set the new password for your account so you can login and access all the feature",
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "New Password",
+                  filled: true,
+                  isDense: true,
+                  fillColor: AppColors.systemGrey3.withOpacity(0.3),
+                  prefixIcon: const Icon(
+                    CupertinoIcons.lock,
+                  )),
+            ),
+            SizedBox(
+              height: AppSizes.bodyPadding,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "Confirm Password",
+                  filled: true,
+                  isDense: true,
+                  fillColor: AppColors.systemGrey3.withOpacity(0.3),
+                  prefixIcon: const Icon(
+                    CupertinoIcons.lock,
+                  )),
+            ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            CupertinoButton.filled(
+                onPressed: () {
+                  Navigator.push(context,
+                      CupertinoPageRoute(builder: (_) => const SignInScreen()));
+                },
+                child: const Text("Reset Password")),
+          ],
+        ),
+      ],
+    ));
+  }
+}
--- lib/views/auth/otp_scr.dart
+++ lib/views/auth/otp_scr.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mk_mining/configs/colors.dart';
 import 'package:mk_mining/configs/sizes.dart';
+import 'package:mk_mining/views/auth/new_password_scr.dart';
 
 class OTPScreen extends StatelessWidget {
   const OTPScreen({super.key});
@@ -16,113 +16,127 @@ class OTPScreen extends StatelessWidget {
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover)),
-        child: Padding(
-          padding: const EdgeInsets.all(AppSizes.bodyPadding),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: AppSizes.bodyPadding),
+          children: [
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            Image.asset(
+              "assets/images/password.png",
+              height: AppSizes.width(context) * 0.25,
+            ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            Column(
               children: [
-                SizedBox(height: AppSizes.height(context) * 0.1,),
-                Image.asset(
-                  "assets/images/password.png",
-                  width: AppSizes.width(context) * 0.3,
+                Text(
+                  "Enter Verification Code",
+                  style:
+                      TextStyle(fontSize: AppSizes.width(context) * 0.07),
                 ),
-                SizedBox(height: AppSizes.height(context) * 0.1,),
-                Column(
-                  children: [
-                    Text(
-                      "Enter Verification Code",
-                      style: TextStyle(fontSize: AppSizes.width(context) * 0.07),
-                    ),
-                    const Text(
-                      "We are automatically detecting a mail sent to your email address ****s01@gmail.com",
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                ),
-                SizedBox(height: AppSizes.height(context) * 0.1,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: AppColors.seed,
-                          borderRadius: BorderRadius.circular(AppSizes.radius)),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "8",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.white,
-                            fontSize: 35),
-                      ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: AppColors.seed.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(AppSizes.radius)),
-                      alignment: Alignment.center,
-                      child: const TextField(
-                        keyboardType: TextInputType.number,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      )
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: AppColors.seed.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(AppSizes.radius)),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.white,
-                            fontSize: 35),
-                      ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: AppColors.seed.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(AppSizes.radius)),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.white,
-                            fontSize: 35),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: AppSizes.height(context) * 0.1,),
-                CupertinoButton(
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't receive the OTP?",
-                          style: TextStyle(color: AppColors.black),
-                        ),
-                        Text(" Resend OTP"),
-                      ],
-                    ),
-                    onPressed: () {}),
-                    
+                const Text(
+                  "We are automatically detecting a mail sent to your email address ****s01@gmail.com",
+                  textAlign: TextAlign.center,
+                )
               ],
             ),
-          ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.seed,
+                      borderRadius: BorderRadius.circular(AppSizes.radius)),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "8",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                        fontSize: 35),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: AppColors.seed.withOpacity(0.5),
+                        borderRadius:
+                            BorderRadius.circular(AppSizes.radius)),
+                    alignment: Alignment.center,
+                    child: const TextField(
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    )),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.seed.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(AppSizes.radius)),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                        fontSize: 35),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.seed.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(AppSizes.radius)),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                        fontSize: 35),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            CupertinoButton.filled(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (_) => const NewPasswordScreen()));
+                },
+                child: const Text("Continue")),
+            SizedBox(
+              height: AppSizes.height(context) * 0.1,
+            ),
+            CupertinoButton(
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't receive the OTP?",
+                      style: TextStyle(color: AppColors.black),
+                    ),
+                    Text(" Resend OTP"),
+                  ],
+                ),
+                onPressed: () {}),
+          ],
         ),
       ),
     );
